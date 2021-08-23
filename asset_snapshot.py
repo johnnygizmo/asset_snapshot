54969c6b25f475969e121bf362bdf46fbc80157e
Johnny Matthews <johnny.matthews@gmail.com>
2021-08-23 10:27:58 -0500

    Create asset_snapshot.py

---
+++ asset_snapshot.py
@@ -0,0 +1,98 @@
+import bpy
+import os
+bl_info = {
+    'name': 'Asset Snapshot',
+    'category': 'View3d',
+    "blender": (3, 0, 0),
+}
+    
+def snapshot(context,ob):
+    #Save some basic settings
+    areatype = context.area.type
+    camera = bpy.context.scene.camera  
+    camerapos = camera.location.copy()
+    camerarot = camera.rotation_euler.copy()
+    hold_x = bpy.context.scene.render.resolution_x
+    hold_y = bpy.context.scene.render.resolution_y 
+    filepath = bpy.context.scene.render.filepath
+    
+    # Change Settings
+    bpy.context.scene.render.resolution_y = 250
+    bpy.context.scene.render.resolution_x = 250
+    if bpy.ops.view3d.camera_to_view.poll():
+        bpy.ops.view3d.camera_to_view()  
+    bpy.context.scene.render.filepath = os.path.join("/tmp", ob.name) 
+    file = os.path.join("/tmp", ob.name)+".png"
+
+    #Render File, Mark Asset and Set Image
+    bpy.ops.render.render(write_still = True)
+    ob.asset_mark()
+    override = bpy.context.copy()
+    context.area.type = 'FILE_BROWSER'
+    override['id'] = ob
+    bpy.ops.ed.lib_id_load_custom_preview(override,filepath=file)
+    
+    #Cleanup
+    context.area.type = areatype
+    os.unlink(file)
+    bpy.context.scene.render.resolution_y = hold_y
+    bpy.context.scene.render.resolution_x = hold_x
+    camera.location = camerapos
+    camera.rotation_euler = camerarot
+    bpy.context.scene.render.filepath = filepath
+    bpy.ops.view3d.view_camera()
+    
+class AssetSnapshotCollection(bpy.types.Operator):
+    """Create a preview of a collection"""
+    bl_idname = "view3d.asset_snaphot_collection"
+    bl_label = "Asset Snapshot - Collection"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        if context.area.type != 'VIEW_3D':
+            return False
+        
+        if context.collection == None:
+            return False
+
+        return True
+
+    def execute(self, context):
+        snapshot(context,context.collection)
+        return {'FINISHED'}
+     
+class AssetSnapshotObject(bpy.types.Operator):
+    """Create an asset preview of an object"""
+    bl_idname = "view3d.object_preview"
+    bl_label = "Asset Snapshot - Object"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        if context.area.type != 'VIEW_3D':
+            return False
+        if context.active_object == None:
+            return False
+
+        return True
+
+    def execute(self, context):
+        print(context.active_object)
+        snapshot(context, context.active_object)
+        return {'FINISHED'}
+
+
+def register():
+    bpy.utils.register_class(AssetSnapshotCollection)
+    bpy.utils.register_class(AssetSnapshotObject)
+
+def unregister():
+    bpy.utils.unregister_class(AssetSnapshotCollection)
+    bpy.utils.unregister_class(AssetSnapshotObject)
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    #bpy.ops.view3d.collection_asset_preview()
