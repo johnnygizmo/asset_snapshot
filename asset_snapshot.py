aed2be50665c7a382020c87f9ca7d616997928f2
LJ3D <65134690+LJ3D@users.noreply.github.com>
2021-08-31 18:05:06 +0100

    Added UI panel

--- asset_snapshot.py
+++ asset_snapshot.py
@@ -1,13 +1,19 @@
 import bpy
-from bpy.types import Operator
+from bpy.types import (Panel,
+                       # Menu,
+                       Operator,
+                       PropertyGroup,
+                       )
 from bpy.props import (
         # FloatProperty,
         IntProperty,
         # BoolProperty,
         # StringProperty,
-        # FloatVectorProperty
+        # FloatVectorProperty,
+        PointerProperty,
         )
 import os
+
 
 bl_info = {
     'name': 'Asset Snapshot',
@@ -17,6 +23,8 @@ bl_info = {
 
 
 def snapshot(self,context,ob):
+    scene = context.scene
+    tool = scene.asset_snapshot
     #Save some basic settings
     areatype = context.area.type
     if bpy.context.scene.camera == None:
@@ -27,25 +35,19 @@ def snapshot(self,context,ob):
     hold_x = bpy.context.scene.render.resolution_x
     hold_y = bpy.context.scene.render.resolution_y 
     filepath = bpy.context.scene.render.filepath
-    
-    
     # Find objects that are hidden in viewport and hide them in render
     tempHidden = []
     for o in bpy.data.objects:
         if o.hide_get() == True:
             o.hide_render = True
             tempHidden.append(o)
-    
-    
     # Change Settings
-    bpy.context.scene.render.resolution_y = self.resolution
-    bpy.context.scene.render.resolution_x = self.resolution
+    bpy.context.scene.render.resolution_y = tool.resolution
+    bpy.context.scene.render.resolution_x = tool.resolution
     if bpy.ops.view3d.camera_to_view.poll():
         bpy.ops.view3d.camera_to_view()  
     bpy.context.scene.render.filepath = os.path.join("/tmp", ob.name) 
     file = os.path.join("/tmp", ob.name)+".png"
-    
-    
     #Render File, Mark Asset and Set Image
     bpy.ops.render.render(write_still = True)
     ob.asset_mark()
@@ -53,13 +55,9 @@ def snapshot(self,context,ob):
     context.area.type = 'FILE_BROWSER'
     override['id'] = ob
     bpy.ops.ed.lib_id_load_custom_preview(override,filepath=file)
-    
-    
     # Unhide the objects hidden for the render
     for o in tempHidden:
         o.hide_render = False
-    
-    
     #Cleanup
     context.area.type = areatype
     os.unlink(file)
@@ -71,18 +69,21 @@ def snapshot(self,context,ob):
     bpy.ops.view3d.view_camera()
 
 
-class AssetSnapshotCollection(Operator):
-    """Create a preview of a collection"""
-    bl_idname = "view3d.asset_snaphot_collection"
-    bl_label = "Asset Snapshot - Collection"
-    bl_options = {'REGISTER', 'UNDO'}
-    resolution: IntProperty(
+class properties(PropertyGroup):
+    resolution : IntProperty(
             name="Preview Resolution",
             description="Resolution to render the preview",
             min=1,
             soft_max=500,
             default=256
             )
+
+
+class AssetSnapshotCollection(Operator):
+    """Create a preview of a collection"""
+    bl_idname = "view3d.asset_snaphot_collection"
+    bl_label = "Asset Snapshot - Collection"
+    bl_options = {'REGISTER', 'UNDO'}
     @classmethod
     def poll(cls, context):
         if context.area.type != 'VIEW_3D':
@@ -100,13 +101,6 @@ class AssetSnapshotObject(Operator):
     bl_idname = "view3d.object_preview"
     bl_label = "Asset Snapshot - Object"
     bl_options = {'REGISTER', 'UNDO'}
-    resolution: IntProperty(
-            name="Preview Resolution",
-            description="Resolution to render the preview",
-            min=1,
-            soft_max=500,
-            default=250
-            )
     @classmethod
     def poll(cls, context):
         if context.area.type != 'VIEW_3D':
@@ -119,13 +113,39 @@ class AssetSnapshotObject(Operator):
         return {'FINISHED'}
 
 
+class OBJECT_PT_panel(Panel):
+    bl_label = "asset_snapshot"
+    bl_idname = "OBJECT_PT_asset_snapshot_panel"
+    bl_category = "asset_snapshot"
+    bl_space_type = "VIEW_3D"   
+    bl_region_type = "UI"
+    @classmethod
+    def poll(self,context):
+        return context.mode
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        tool = scene.asset_snapshot
+        layout.prop(tool, "resolution")
+        layout.operator("view3d.object_preview")
+        layout.operator("view3d.asset_snaphot_collection")
+
+
 def register():
+    bpy.utils.register_class(properties)
     bpy.utils.register_class(AssetSnapshotCollection)
     bpy.utils.register_class(AssetSnapshotObject)
+    bpy.utils.register_class(OBJECT_PT_panel)
+    
+    bpy.types.Scene.asset_snapshot = PointerProperty(type=properties)
 
 def unregister():
+    bpy.utils.unregister_class(properties)
     bpy.utils.unregister_class(AssetSnapshotCollection)
     bpy.utils.unregister_class(AssetSnapshotObject)
+    bpy.utils.unregister_class(OBJECT_PT_panel)
+    
+    del bpy.types.Scene.asset_snapshot
 
 if __name__ == "__main__":
     register()
